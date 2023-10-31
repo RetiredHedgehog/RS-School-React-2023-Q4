64fec080dbc929f263e67621bb5c4e8bcba0d192
retiredhedgehog <lukashov9999@gmail.com>
2023-10-31 19:17:31 +0300

    refactor: convert class component to function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Search from './components/Search/Search';
 import Display from './components/Display/Display';
@@ -8,96 +8,109 @@ import NamedApiResource from './types/namedAPIResource';
 import helpers from './helpers';
 import ErrorButton from './components/ErrorButton';
 
-type State = {
-  page: NamedEndpointResponse<NamedApiResource> | null;
-  limit: number;
-  offset: number;
-  searchText: string;
-  error: Error | null;
-};
+const App = () => {
+  const [limit] = useState(10);
+  const [offset] = useState(0);
+  const [page, setPage] =
+    useState<NamedEndpointResponse<NamedApiResource> | null>(null);
+  const [searchText, setSearchText] = useState(helpers.getSearchText());
+  const [error, setError] = useState<Error | null>(null);
 
-class App extends Component {
-  state: State = {
-    page: null,
-    limit: 10,
-    offset: 0,
-    searchText: helpers.getSearchText(),
-    error: null,
-  };
+  useEffect(() => {
+    const searchText = helpers.getSearchText();
+    const controller = new AbortController();
 
-  async componentDidMount(): Promise<void> {
-    const text = helpers.getSearchText();
-    if (!text) {
-      const page = await pokemonService.getPokemons(0, 10);
-      this.setState({ page });
+    if (!searchText) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/?${new URLSearchParams({
+              offset: offset.toString(),
+              limit: limit.toString(),
+            })}`,
+            {
+              signal: controller.signal,
+            }
+          );
+          if (!response.ok) {
+            throw new Error(`${response.status} ${response.statusText}`);
+          }
+          const page: NamedEndpointResponse<NamedApiResource> =
+            await response.json();
+
+          setPage(page);
+        } catch (error: unknown) {
+          if (typeof error === 'string') {
+            setError(new Error(error));
+            return;
+          }
+
+          if (error instanceof Error && error.name !== 'AbortError') {
+            setError(error);
+            return;
+          }
+        }
+      };
+
+      fetchData();
       return;
     }
 
-    this.setState({
-      page: {
+    setPage({
+      count: 1,
+      next: null,
+      previous: null,
+      results: [
+        {
+          name: searchText,
+          url: `https://pokeapi.co/api/v2/pokemon/${searchText}`,
+        },
+      ],
+    });
+
+    return () => controller.abort();
+  }, [offset, limit]);
+
+  const handleSearchButtonClick = async (): Promise<void> => {
+    helpers.saveSearchText(searchText);
+
+    if (searchText === '') {
+      const page = await pokemonService.getPokemons(0, 10);
+      setPage(page);
+    } else {
+      setPage({
         count: 1,
         next: null,
         previous: null,
         results: [
           {
-            name: text,
-            url: `https://pokeapi.co/api/v2/pokemon/${text}`,
+            name: searchText,
+            url: helpers.getPokemonUrl(searchText),
           },
         ],
-      },
-    });
-  }
-
-  handleSearchButtonClick = async (): Promise<void> => {
-    const searchText = this?.state.searchText || '';
-
-    helpers.saveSearchText(searchText);
-
-    if (searchText === '') {
-      const page = await pokemonService.getPokemons(0, 10);
-      this.setState({ page: page });
-    } else {
-      this.setState({
-        page: {
-          count: 1,
-          next: null,
-          previous: null,
-          results: [
-            {
-              name: searchText,
-              url: helpers.getPokemonUrl(searchText),
-            },
-          ],
-        },
       });
     }
   };
 
-  onInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-    this.setState({
-      searchText: e.currentTarget.value,
-    });
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setSearchText(e.currentTarget.value);
   };
 
-  setPage = (page: NamedEndpointResponse<NamedApiResource>) => {
-    this.setState({
-      page,
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <ErrorButton />
-        <Search
-          searchText={this.state.searchText}
-          onClick={this.handleSearchButtonClick}
-          onInputChange={this.onInputChange}
-        />
-        <Display page={this.state.page} />
-      </>
-    );
+  if (error) {
+    throw error;
   }
-}
+
+  return (
+    <>
+      <ErrorButton />
+      <Search
+        searchText={searchText}
+        onClick={handleSearchButtonClick}
+        onInputChange={handleInputChange}
+      />
+      <Display page={page} />
+    </>
+  );
+};
 
 export default App;
